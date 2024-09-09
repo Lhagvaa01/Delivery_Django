22462010db29a6924f3d40cbb9ba753f1bd24aeb
Lhagvaa <zayalhagva6@gmail.com>
2024-09-09 11:02:51 +0800

    Connect Mqsql

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -88,12 +88,24 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'delivery',
+        'USER': 'dotood',
+        'PASSWORD': 'kacc@9002Aa',
+        'HOST': '66.181.175.153',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
