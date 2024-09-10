eeb65229b1588107b1a79bea16b1babc2c6fdb5a
Lhagvaa <zayalhagva6@gmail.com>
2024-09-10 14:55:04 +0800

    Neon db5

--- delivery/views.py
+++ delivery/views.py
@@ -112,7 +112,7 @@ class HistoryView(APIView):
         serializer = HistorySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({'message': 'ok', 'body':serializer.data} , status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'POST'])
